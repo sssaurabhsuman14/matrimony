8621aa34680f8d0609d80e36c326de1e576ae242
monikanidhisingh <monikanidhisingh@gmail.com>
2019-07-23 17:28:01 +0530

    registrationcontroller usermodel,validation updated

--- src/main/java/com/hcl/matrimony/validation/Validation.java
+++ src/main/java/com/hcl/matrimony/validation/Validation.java
@@ -1,19 +1,20 @@
 package com.hcl.matrimony.validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import com.hcl.matrimony.entity.MyLike;
 import com.hcl.matrimony.exception.ApplicationException;
-
+import com.hcl.matrimony.model.UserModel;
 
 @Component
-public class Validation 
-{
-	 
-	public static void validateLoginRequest(String email,String password) throws ApplicationException 
-	{		
-		if(!StringUtils.hasText(password) || !StringUtils.hasText(email))
+public class Validation {
+
+	public static void validateLoginRequest(String email, String password) throws ApplicationException {
+		if (!StringUtils.hasText(password) || !StringUtils.hasText(email))
 			throw new ApplicationException("Please enter mandatory fields");
 
 	}
@@ -25,5 +26,24 @@ public class Validation
 		return false;
 	}
 
-	
+	public static void validateUser(UserModel user) throws ApplicationException {
+		if (!StringUtils.hasText(user.getUserName()) || !StringUtils.hasText(user.getReligion()) || !StringUtils.hasText(user.getPassword())
+				|| !StringUtils.hasText(user.getMotherTounge()) || user.getMobileNo()==null || !StringUtils.hasText(user.getMaritalStatus())
+				|| !StringUtils.hasText(user.getHeight()) || !StringUtils.hasText(user.getEmail()) || user.getDateOfBirth()==null
+				|| !StringUtils.hasText(user.getCity()) || user.getAge()==null) {
+			throw new ApplicationException("Please fill all the details");
+
+		}
+	}
+
+	public static void validateEmail(UserModel user) throws ApplicationException {
+
+		String regex = "^(.+)@(.+)$";
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(user.getEmail());
+		if(!matcher.matches()) 
+			throw new ApplicationException("Please entre vaild email id");
+	}
+
 }
