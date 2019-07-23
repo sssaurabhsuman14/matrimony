d284ed10dd859cb631ec77f326cfb19e98cf6658
Priyanka-code <priyankasarkate@gmail.com>
2019-07-23 17:47:24 +0530

    search model adding userid

--- src/main/java/com/hcl/matrimony/controller/SearchController.java
+++ src/main/java/com/hcl/matrimony/controller/SearchController.java
@@ -12,35 +12,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hcl.matrimony.entity.User;
 import com.hcl.matrimony.exception.ApplicationException;
-import com.hcl.matrimony.exception.RecordNotFoundException;
 import com.hcl.matrimony.model.ResponseData;
 import com.hcl.matrimony.model.SearchModel;
-import com.hcl.matrimony.model.UserModel;
 import com.hcl.matrimony.service.UserService;
 
 @Controller
 @RequestMapping("/search/{age}/{height}/{religion}/{city}/{maritalStatus}")
 public class SearchController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@GetMapping
-	public ResponseEntity<?> searchProfile(@PathVariable(value = "age") Integer age, @PathVariable(value = "height") String height, @PathVariable(value = "religion") String religion, @PathVariable(value = "city") String city, @PathVariable(value = "maritalStatus") String maritalStatus) throws ApplicationException{
-		
+	public ResponseEntity<ResponseData> searchProfile(@PathVariable(value = "age") Integer age, @PathVariable(value = "height") String height, @PathVariable(value = "religion") String religion, @PathVariable(value = "city") String city, @PathVariable(value = "maritalStatus") String maritalStatus) throws ApplicationException{
+
 		SearchModel searchModel = new SearchModel();
 		searchModel.setAge(age);
 		searchModel.setHeight(height);
 		searchModel.setReligion(religion);
 		searchModel.setCity(city);
 		searchModel.setMaritalStatus(maritalStatus);
-		
-		List<User> userList = userService.searchProfile(searchModel);
-		
-		if (!userList.isEmpty()) {
-		ResponseData response = new ResponseData("Please find below profiles based on search parameters " , HttpStatus.OK, userList);
 
-		return new ResponseEntity (response ,HttpStatus.OK);
+		List<User> userList = userService.searchProfile(searchModel);
+
+		if (!userList.isEmpty()) {
+			ResponseData response = new ResponseData("Please find below profiles based on search parameters " , HttpStatus.OK, userList);
+
+			return new ResponseEntity<ResponseData> (response ,HttpStatus.OK);
 		}
 		throw new ApplicationException("No matching profiles found with specified criteria.");
 	}
--- src/main/java/com/hcl/matrimony/repository/UserRepository.java
+++ src/main/java/com/hcl/matrimony/repository/UserRepository.java
@@ -1,25 +1,27 @@
 package com.hcl.matrimony.repository;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.hcl.matrimony.entity.User;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-	
+
 	public List<User> findByReligion(String religion);
-	
+
 	public List<User> findByHeight(Integer height);
-	
+
 	public List<User> findByCity(String city);
 
 	public List<User> findByMaritalStatus(String maritalStatus);
 
 	public Optional<User> findByEmail(String email);
-	
+
 	@Query(value="select * from user where age = :age and height = :height and religion= :religion"
 			+ " and city = :city and marital_status = :maritalStatus ", nativeQuery = true)
 	List<User> searchProfiles (int age, String height, String religion, String city, String maritalStatus);
